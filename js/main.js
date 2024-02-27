45d7b42c453f72422ad0c3e04af4d5fb5c8773bd
jonathan skarman <kaboomgf@gmail.com>
2024-02-27 09:49:44 +0100

    formated documents

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-document.querySelector(".menu-button").addEventListener("click",showMenu)
-document.querySelector(".fade-layer").addEventListener("click",showMenu)
+document.querySelector(".menu-button").addEventListener("click", showMenu);
+document.querySelector(".fade-layer").addEventListener("click", showMenu);
 
-function showMenu(){
-	let menu = document.querySelector("nav.menu");
-	menu.classList.toggle("show");
+function showMenu() {
+  let menu = document.querySelector("nav.menu");
+  menu.classList.toggle("show");
 
-	let layer = document.querySelector(".fade-layer");
-	layer.classList.toggle("visible");
+  let layer = document.querySelector(".fade-layer");
+  layer.classList.toggle("visible");
 }
 /*hela denna js är snodd från uppgiften där vi byggde en hamburgarmeny*/
